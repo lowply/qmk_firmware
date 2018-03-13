44a1905f8f182099a137a139ee080c7736603d0f
Sho Mizutani <lowply@github.com>
2018-03-14 02:37:22 +0900

    Update keymap

--- keyboards/ergodox_ez/keymaps/lowply/keymap.c
+++ keyboards/ergodox_ez/keymaps/lowply/keymap.c
@@ -18,18 +18,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------. 
- * | ESC    |   `  |   1  |   2  |   3  |   4  |  5   |           |  6   |   7  |   8  |   9  |   0  |   -  | BkSpc  |
+ * | `      |   1  |   2  |   3  |   4  |   5  |  6   |           |  7   |   8  |   9  |   0  |   -  |   =  | BkSpc  |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  | LANG |           | LANG |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | Ctrl   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  | Enter  |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------|      |           |  B   |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   | LYR  |      |      | Opt  |  Cmd |                                       | Cmd  |   '  |   [  |   ]  | RESET|
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        |      |      |       | LEFT | RIGHT|
+ *                                        |  ESC |      |       | LEFT | RIGHT|
  *                                 ,------|------|------|       |------+------+------.
  *                                 |      |      |      |       | UP   |      |      |
  *                                 | Space|      |------|       |------|      |Space |
@@ -40,22 +40,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
-        KC_ESC,   KC_GRV,   KC_1,   KC_2,    KC_3,     KC_4,  KC_5,
-        KC_TAB,   KC_Q,     KC_W,   KC_E,    KC_R,     KC_T,  KC_LANG2,
-        KC_LCTL,  KC_A,     KC_S,   KC_D,    KC_F,     KC_G,
-        KC_LSFT,  KC_Z,     KC_X,   KC_C,    KC_V,     KC_B,  KC_NO,
-        MO(1),    KC_NO,    KC_NO,  KC_LALT, KC_LGUI,
+        KC_GRV,   KC_1,    KC_2,   KC_3,     KC_4,     KC_5,  KC_6,
+        KC_TAB,   KC_Q,    KC_W,   KC_E,     KC_R,     KC_T,  KC_LANG2,
+        KC_LCTL,  KC_A,    KC_S,   KC_D,     KC_F,     KC_G,
+        KC_LSFT,  KC_Z,    KC_X,   KC_C,     KC_V,     KC_B,  KC_NO,
+        MO(1),    KC_NO,   KC_NO,  KC_LALT,  KC_LGUI,
 		// left bottom
-			    KC_NO, KC_NO,
-					   KC_NO,
-		KC_SPC, KC_NO, KC_NO,
+			    KC_ESC, KC_NO,
+					    KC_NO,
+		KC_SPC, KC_NO,  KC_NO,
 
         // right hand
-        KC_7,      KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS,   KC_BSPC,
-        KC_LANG1,  KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,      KC_BSLS,
-                   KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN,   KC_ENT,
-        KC_NO,     KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH,   KC_RSFT,
-                        KC_RGUI, KC_QUOT, KC_LBRC, KC_RBRC,   RESET,
+        KC_7,      KC_8,   KC_9,    KC_0,    KC_MINS,   KC_EQL,    KC_BSPC,
+        KC_LANG1,  KC_Y,   KC_U,    KC_I,    KC_O,      KC_P,      KC_BSLS,
+                   KC_H,   KC_J,    KC_K,    KC_L,      KC_SCLN,   KC_ENT,
+        KC_B,      KC_N,   KC_M,    KC_COMM, KC_DOT,    KC_SLSH,   KC_RSFT,
+                           KC_RGUI, KC_QUOT, KC_LBRC,   KC_RBRC,   RESET,
 		// right bottom
         KC_LEFT, KC_RIGHT,
         KC_UP,
