ad0925302e4db6b87ef703cf0f7a60a36661cd53
Sho Mizutani <lowply@github.com>
2018-03-25 01:50:46 +0900

    Update keymap

--- flush.sh
+++ flush.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 make ergodox_ez:lowply
-teensy_loader_cli --mcu=atmega32u4 -s -v .build/ergodox_ez_lowply.hex
+# teensy_loader_cli --mcu=atmega32u4 -s -v .build/ergodox_ez_lowply.hex
--- keyboards/ergodox_ez/keymaps/lowply/keymap.c
+++ keyboards/ergodox_ez/keymaps/lowply/keymap.c
@@ -20,10 +20,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------. 
  * | `      |   1  |   2  |   3  |   4  |   5  |  6   |           |  7   |   8  |   9  |   0  |   -  |   =  | BkSpc  |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   E  |   R  |   T  | LANG |           | LANG |   Y  |   U  |   I  |   O  |   P  |   \    |
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  |   H  |           |  G   |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | Ctrl   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  | Enter  |
- * |--------+------+------+------+------+------|      |           |  B   |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------|   N  |           |  B   |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   | LYR  |      |      | Opt  |  Cmd |                                       | Cmd  |   '  |   [  |   ]  |   `  |
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                        |  ESC |      |       | LEFT | RIGHT|
  *                                 ,------|------|------|       |------+------+------.
  *                                 |      |      |      |       | UP   |      |      |
- *                                 | Space|      |------|       |------|      |Space |
+ *                                 | Space| LANG |------|       |------| LANG |Space |
  *                                 |      |      |      |       | DOWN |      |      |
  *                                 `--------------------'       `--------------------'
  */
@@ -41,25 +41,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
         KC_GRV,   KC_1,    KC_2,   KC_3,     KC_4,     KC_5,  KC_6,
-        KC_TAB,   KC_Q,    KC_W,   KC_E,     KC_R,     KC_T,  KC_LANG2,
+        KC_TAB,   KC_Q,    KC_W,   KC_E,     KC_R,     KC_T,  KC_H,
         KC_LCTL,  KC_A,    KC_S,   KC_D,     KC_F,     KC_G,
-        KC_LSFT,  KC_Z,    KC_X,   KC_C,     KC_V,     KC_B,  KC_NO,
+        KC_LSFT,  KC_Z,    KC_X,   KC_C,     KC_V,     KC_B,  KC_N,
         MO(1),    KC_NO,   KC_NO,  KC_LALT,  KC_LGUI,
         // left bottom
-                KC_ESC, KC_NO,
-                        KC_NO,
-        KC_SPC, KC_NO,  KC_NO,
+                KC_ESC,    KC_NO,
+                           KC_NO,
+        KC_SPC, KC_LANG2,  KC_NO,
 
         // right hand
-        KC_7,      KC_8,   KC_9,    KC_0,    KC_MINS,   KC_EQL,    KC_BSPC,
-        KC_LANG1,  KC_Y,   KC_U,    KC_I,    KC_O,      KC_P,      KC_BSLS,
-                   KC_H,   KC_J,    KC_K,    KC_L,      KC_SCLN,   KC_ENT,
-        KC_B,      KC_N,   KC_M,    KC_COMM, KC_DOT,    KC_SLSH,   KC_RSFT,
-                           KC_RGUI, KC_QUOT, KC_LBRC,   KC_RBRC,   KC_GRV,
+        KC_7,  KC_8,   KC_9,    KC_0,    KC_MINS,   KC_EQL,    KC_BSPC,
+        KC_G,  KC_Y,   KC_U,    KC_I,    KC_O,      KC_P,      KC_BSLS,
+               KC_H,   KC_J,    KC_K,    KC_L,      KC_SCLN,   KC_ENT,
+        KC_B,  KC_N,   KC_M,    KC_COMM, KC_DOT,    KC_SLSH,   KC_RSFT,
+                       KC_RGUI, KC_QUOT, KC_LBRC,   KC_RBRC,   KC_GRV,
         // right bottom
         KC_LEFT, KC_RIGHT,
         KC_UP,
-        KC_DOWN, KC_NO, KC_SPC
+        KC_DOWN, KC_LANG1, KC_SPC
     ),
 /* Keymap 1: Symbol Layer
  *
@@ -96,11 +96,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_TRNS, KC_TRNS,
 
         // right hand
-        KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,  KC_TRNS,
-        KC_PGUP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
-                 KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_TRNS, KC_TRNS,
-        KC_PGDN, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
-                          KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, RESET,
+        KC_F7,   KC_F8,           KC_F9,   KC_F10,  KC_F11,           KC_F12,  KC_TRNS,
+        KC_PGUP, SGUI(KC_LBRC), KC_TRNS, KC_TRNS, SGUI(KC_RBRC),  KC_TRNS, KC_TRNS,
+                 KC_LEFT,         KC_DOWN, KC_UP,   KC_RIGHT,         KC_TRNS, KC_TRNS,
+        KC_PGDN, KC_TRNS,         KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS,
+                                  KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, RESET,
         // right bottom
         KC_TRNS, KC_TRNS,
         KC_TRNS,
